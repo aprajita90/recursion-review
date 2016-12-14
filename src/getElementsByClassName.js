2b18ce37dd2993d2918725d25e4c3def6dcad873
aprajita_jain <aprajitajain90@gmail.com>
2016-12-14 06:51:40 -0800

    Finished two file

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+    // your code here
+    var results = [];
+
+    var getToChildNode = function(node) {
+        if (node.classList.contains(className)) {//node.className.split(" ").indexOf()(className)>=0
+            results.push(node);
+        }
+        for (var i = 0; i < node.children.length; i++) {
+            getToChildNode(node.children[i]);//recursive over child
+        }
+    };
+    getToChildNode(document.body);//recurse first time
+
+    return results;//retur saved the mode
 };
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,28 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+    // your code goes here
+    var arrKeys = [];
+    if (obj === null) {
+        return 'null';
+    } else if (typeof obj === "number" || typeof obj === "boolean") {
+        return '' + obj;
+    } else if (typeof obj === "string") {
+        return '"' + obj + '"'
+    } else if (Array.isArray(obj)) {
+        return '[' + obj.map(function(element) {
+            if (typeof element === 'undefined' || typeof element === 'function') {
+                return 'null';
+            }
+            return stringifyJSON(element);
+        }) + ']';
+
+    } else if (obj instanceof Object) {
+        Object.keys(obj).forEach(function(key) {
+            if (typeof obj[key] === 'undefined' || typeof obj[key] === 'function') {} else {
+                arrKeys.push('"' + key + '":' + stringifyJSON(obj[key]));
+            }
+        });
+        return '{' + arrKeys + '}';
+    }
 };
